c18e40afd7729ea305ca2d4c7604fc3e561844fe
Kislay Aditya Oj <oasiskislay@gmail.com>
2023-12-09 21:25:00 +0530

    pulled new assignments

--- Week 1 /assignment/easy/expenditure-analysis.js
+++ Week 1 /assignment/easy/expenditure-analysis.js
@@ -5,7 +5,9 @@
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
 
-function calculateTotalSpentByCategory(transactions) {
+function calculateTotalSpentByCategory(transactions) 
+{
+  
   return [];
 }
 
